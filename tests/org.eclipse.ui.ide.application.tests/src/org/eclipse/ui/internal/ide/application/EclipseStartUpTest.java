1ce6658d56428d57f1b044f252031fb1b6d4d009
Michael Bangas <michael.bangas@vector.com>
2025-01-02 11:06:47 +0100

    add PerformanceTest
    Reapply "added all SVGs"
    
    This reverts commit 2215db19c6dd05374ba9abf3d6a233126ec8bd05.
    
    Revert "added all SVGs"
    
    This reverts commit c06a7b94d13c55ffa2e2cbfe0fe345c311d5e9ee.

--- tests/org.eclipse.ui.ide.application.tests/src/org/eclipse/ui/internal/ide/application/EclipseStartUpTest.java
+++ tests/org.eclipse.ui.ide.application.tests/src/org/eclipse/ui/internal/ide/application/EclipseStartUpTest.java
@@ -1,37 +1,60 @@
 package org.eclipse.ui.internal.ide.application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.junit.Test;
 
-/**
- * @since 3.5
- *
- */
 public class EclipseStartUpTest {
 
-	static long endTime;
+	static long endTime = 0;
 
 	@Test
-	public void testStandardUIStartupTime() throws Exception {
+	public void testStandardUIStartupTime() {
+		int iterations = 1;
+		String logFile = "D:\\dev\\oomph\\Bachelor\\startup_times_PNG_125.txt";
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))) {
+			for (int i = 1; i <= iterations; i++) {
+				long elapsedTime = measureStandardUIStartupTime();
+				writer.write(elapsedTime + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private long measureStandardUIStartupTime() {
 		endTime = 0;
+		Display display = PlatformUI.createDisplay();
 		long startTime = System.currentTimeMillis();
-		PlatformUI.createAndRunWorkbench(PlatformUI.createDisplay(), new WorkbenchAdvisor() {
+		PlatformUI.createAndRunWorkbench(display, new IDEWorkbenchAdvisor(100, null) {
+
+			@Override
+			public void postStartup() {
+				super.postStartup();
+
+			}
 
 			@Override
 			public void eventLoopIdle(Display display) {
+				super.eventLoopIdle(display);
 				if (endTime == 0) {
 					endTime = System.currentTimeMillis();
 				}
+				display.execute(() -> {
+					PlatformUI.getWorkbench().close();
+				});
 			}
 
 			@Override
 			public String getInitialWindowPerspectiveId() {
-				return "org.eclipse.ui.resourcePerspective";
+				return "org.eclipse.debug.ui.DebugPerspective";
 			}
 		});
-		long elapsedTime = endTime - startTime;
-		System.out.println("Eclipse UI startup time: " + elapsedTime + " ms");
+		return endTime - startTime;
 	}
 }
